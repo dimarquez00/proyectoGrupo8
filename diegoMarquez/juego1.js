f373ee9570c9641aea9d431bbeb71eed1da0d91e
dimarquez00 <dimarquez@uade.edu.ar>
2024-06-07 18:50:24 -0300

    Update language attribute to Spanish in HTML files

---
+++ diegoMarquez/juego1.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+    var firstCard = null;
+    var secondCard = null;
+    var lockBoard = false;
+    var signal = null;
+
+    $('.card').click(function() {
+        if (lockBoard) return;
+        if ($(this).hasClass('flipped')) return;
+
+        $(this).html('<img src="./img/' + $(this).data('signal') + '.png">');
+        $(this).addClass('flipped');
+
+        if (!firstCard) {
+            firstCard = $(this);
+        } else {
+            secondCard = $(this);
+            lockBoard = true;
+
+            if (firstCard.data('signal') === secondCard.data('signal')) {
+                $('#memory-result').text('¡Correcto! Has encontrado una pareja.');
+                $('#memory-result').css('color', 'green');
+                // $('.señal1').show();
+                if (firstCard.data('signal') === 'señal1'){
+                    $('#señalDescripcion').html('<p><b>Escolares y niños:</b> Se colocan cercanas a colegios y parques para tener precaución con la velocidad en el trayecto pues circulan infantes.</p>');
+                    $('#señalDescripcion').css('color', 'black');
+                    console.log('Entra en IF');
+                } else if (firstCard.data('signal') === 'señal2') {
+                    $('#señalDescripcion').html('<p><b>Cruce de peatones:</b> SSon áreas dispuestas para que los peatones puedan circular, generalmente se ubican en donde existen semáforos y los conductores deben respetar este espacio.</p>');
+                    $('#señalDescripcion').css('color', 'black');
+                    console.log('Entra en IF2');
+                } else if (firstCard.data('signal') === 'señal3') {
+                    $('#señalDescripcion').html('<p><b>Pare:</b> Indica que los conductores deben detener completamente su vehículo y ceder el paso antes de continuar.</p>');
+                    $('#señalDescripcion').css('color', 'black');
+                    console.log('Entra en IF2');
+                } else if (firstCard.data('signal') === 'señal4') {
+                    $('#señalDescripcion').html('<p><b>Prohibición de circular (Peatones):</b> Indica que los peatones no pueden transitar por esa zona específica, generalmente se encuentra en áreas peligrosas o reservadas exclusivamente para vehículos.</p>');
+                    $('#señalDescripcion').css('color', 'black');
+                    console.log('Entra en IF2');
+                }
+                resetCards(true);
+
+            } else {
+                $('#memory-result').text('Incorrecto. Inténtalo de nuevo.');
+                $('#memory-result').css('color', 'red');
+                setTimeout(resetCards, 1000, false);
+            }
+        }
+        if ($('.card.flipped').length === $('.card').length) {
+            $('#myModal').modal('show');
+        }
+    });
+
+    function resetCards(isMatch) {
+        if (!isMatch) {
+            firstCard.text('❓').removeClass('flipped');
+            secondCard.text('❓').removeClass('flipped');
+        }
+        firstCard = null;
+        secondCard = null;
+        lockBoard = false;
+    }
+});
