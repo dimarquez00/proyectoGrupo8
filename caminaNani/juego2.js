9b47e63b8a5e5e599722eee9be9e6a928c032e57
camila <caminani24@gmail.com>
2024-06-10 22:59:22 -0300

    agrego juego y estilo

--- caminaNani/juego2.js
+++ caminaNani/juego2.js
@@ -1,66 +1,134 @@
-const replaceAt = (string, character,index) =>{
-    return string.substring(0,index) + character + string.substring(index + character.length)
-}
+const replaceAt = (string, character, index) => {
+  return (
+    string.substring(0, index) +
+    character +
+    string.substring(index + character.length)
+  );
+};
 const updateAhorcadoImage = (mistakes) => {
-    let imageElement = document.getElementById('imagenAhorcado');
-    imageElement.src = `./img/ahorcado/${mistakes}.jpg`;
-}
+  let imageElement = document.getElementById("imagenAhorcado");
+  imageElement.src = `./img/ahorcado/${mistakes}.jpg`;
+};
 
 const items = [
-    { image: "./img/señales/bicicleta.png", word: "prohibicion de circulacion" },
-    { image: "./img/señales/contramano.jpg", word: "contramano" },
-    { image: "./img/señales/escolares.jpg", word: "escolares" },
-    { image: "./img/señales/noAvanzar.jpg", word: "no avanzar" },
-    { image: "./img/señales/noEstacionar.jpg", word: "prohibido estacionar" },
-    { image: "./img/señales/noRuidosMolestos.jpg", word: "no ruidos molestos" },
-    { image: "./img/señales/pare.jpg", word: "pare" },
-
+  // { image: "./img/señales/bicicleta.png", word: "prohibicion de circulacion", meaning: "Esta señal significa que no está permitido que las bicicletas circulen por esa área" },"
+  //{ image: "./img/señales/contramano.jpg", word: "contramano", meaning: "Esta señal indica que la vía ante la cual se encuentra tiene sentido de circulación opuesto y por lo tanto no se puede ingresar."},
+  // { image: "./img/señales/escolares.jpg", word: "escolares", meaning: "Esta señal nos indica que hay una escuela cerca y que debemos estar atentos porque puede haber muchos niños caminando por allí" },
+  // { image: "./img/señales/noAvanzar.jpg", word: "no avanzar", meaning:" Esta señal nos dice que debemos detenernos y no seguir adelante" },
+  //{ image: "./img/señales/noEstacionar.jpg", word: "prohibido estacionar", meaning:"Esta señal nos dice que no podemos estacionar nuestros coches en ese lugar." },
+  //{ image: "./img/señales/noRuidosMolestos.jpg", word: "no ruidos molestos", meaning:"Esta señal nos indica que no debemos hacer ruidos fuertes que puedan molestar a otras personas que viven cerca" },
+  {
+    image: "./img/señales/pare.jpg",
+    word: "pare",
+    meaning: "Esta señal nos indica que debemos detenernos por completo.",
+  },
 ];
+
+var myModal = document.getElementById("myModal");
+var modalBody = myModal.querySelector(".modal-body");
+var exampleModalLabel = document.getElementById("exampleModalLabel");
+var username = localStorage.getItem("username") || "Usuario";
+document.getElementById("letterInput").addEventListener("keypress", function(event) {
+    if (event.key >= '0' && event.key <= '9') {
+      event.preventDefault();
+    }
+  });
+
 
 const selectedItem = items[Math.floor(Math.random() * items.length)];
 const secretWord = selectedItem.word;
-let hiddenWord = secretWord.replace(/[^\s]/g, "_ ");
-console.log(secretWord)
-console.log(hiddenWord)
-document.querySelector('.hiddenWord').innerHTML = hiddenWord.replace("  ", " &nbsp;");
-console.log(hiddenWord)
+const meaning = selectedItem.meaning;
+let hiddenWord = secretWord.replace(/[^\s]/g, "_ ").replaceAll("  ", "   ");
+console.log(secretWord);
+console.log(hiddenWord);
+document.querySelector(".hiddenWord").innerHTML = hiddenWord.replaceAll(
+  "  ",
+  " &nbsp;"
+);
+console.log(hiddenWord);
 errorCounter = 0;
 const maxErrors = 7;
 
-document.getElementById('imageToGuess').src = selectedItem.image;
-console.log(document.querySelector('.hiddenWord').innerHTML)
+document.getElementById("imageToGuess").src = selectedItem.image;
+console.log(document.querySelector(".hiddenWord").innerHTML);
 
+const evaluateWord = () => {
+  let error = true;
+  const letter = document.querySelector(".letterInput").value.toLowerCase();
+  if(!/^[a-zA-Z]$/.test(letter)) {
+    document.querySelector(".letterInput").value = ''
+    return
+  }
 
-const evaluateWord = () =>{
-    let error = true
-    const letter = document.querySelector('.letterInput').value;
-    
-    for(let i=0; i<secretWord.length; i++){
-        if(secretWord[i] === letter){
-            hiddenWord = replaceAt(hiddenWord, letter, i * 2)
-            error = false;
-        }
+  for (let i = 0; i < secretWord.length; i++) {
+    if (secretWord[i] === letter) {
+      hiddenWord = replaceAt(hiddenWord, letter, i * 2);
+      error = false;
     }
-    document.querySelector('.hiddenWord').innerHTML = hiddenWord;
-    console.log(document.querySelector('.hiddenWord').innerHTML)
+  }
+  document.querySelector(".hiddenWord").innerHTML = hiddenWord.replaceAll(
+    "  ",
+    " &nbsp;"
+  );
+  console.log(document.querySelector(".hiddenWord").innerHTML);
 
-    if(error){
-        console.log("error")
-        errorCounter++;
-        updateAhorcadoImage(errorCounter);
-    }
+  if (error) {
+    console.log("error");
+    errorCounter++;
+    updateAhorcadoImage(errorCounter);
+  }
 
-    if(!hiddenWord.includes("_")){
-        document.getElementById('evaluateButton').disabled = true;
-        alert("Has ganado")
-    }
-    else if (errorCounter >= maxErrors) {
-        updateAhorcadoImage(errorCounter);
-        setTimeout(() => {
-            alert(`Has perdido. La palabra era: ${secretWord}`);
-            document.getElementById('evaluateButton').disabled = true;
-        }, 100)
-    }
-}
+  // Verifica si la palabra oculta ha sido completada
+  if (!hiddenWord.includes("_")) {
+    evaluateButton.disabled = true;
 
-document.getElementById('evaluateButton').addEventListener('click', evaluateWord)
+    // Cambia el título del modal
+    exampleModalLabel.textContent = "¡Ganaste!";
+
+    // Limpia el contenido actual del cuerpo del modal
+    modalBody.innerHTML = "";
+
+    // Agrega el mensaje de felicitaciones al cuerpo del modal
+    const congratulationsMessage = document.createElement("p");
+    congratulationsMessage.textContent = "¡Felicitaciones, " + username + "!";
+
+    // Agrega la palabra seleccionada y su significado al cuerpo del modal
+    const wordMessage = document.createElement("p");
+    wordMessage.textContent = "La palabra era: " + secretWord + ". " + meaning;
+
+    // Agrega los elementos al cuerpo del modal
+    modalBody.appendChild(congratulationsMessage);
+    modalBody.appendChild(wordMessage);
+
+    // Muestra el modal
+    var bootstrapModal = new bootstrap.Modal(myModal);
+    bootstrapModal.show();
+  } else if (errorCounter >= maxErrors) {
+    updateAhorcadoImage(errorCounter);
+    setTimeout(() => {
+      exampleModalLabel.textContent = "Perdiste!";
+
+      modalBody.innerHTML = "";
+
+      const congratulationsMessage = document.createElement("p");
+      congratulationsMessage.textContent = "¡Felicitaciones, " + username + "!";
+
+      const wordMessage = document.createElement("p");
+      wordMessage.textContent =
+        "La palabra era: " + secretWord + ". " + meaning;
+
+      modalBody.appendChild(congratulationsMessage);
+      modalBody.appendChild(wordMessage);
+
+      var bootstrapModal = new bootstrap.Modal(myModal);
+      bootstrapModal.show();
+      document.getElementById("evaluateButton").disabled = true;
+    }, 100);
+  }
+
+  document.querySelector(".letterInput").value = "";
+};
+
+document
+  .getElementById("evaluateButton")
+  .addEventListener("click", evaluateWord);
