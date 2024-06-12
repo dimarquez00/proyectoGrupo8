3b649e30678ce8e0a7ed9bddddb2ce3fe67ee20b
IanRebuffo <164431040+IanRebuffo@users.noreply.github.com>
2024-06-12 03:04:33 -0300

    Juego3 agregado.

---
+++ ianRebuffo/baseDePreguntas.js
@@ -0,0 +1,52 @@
+let baseDePreguntas = [
+    {
+      imagen: "img/stop.png",
+      pregunta: "¿Qué significa esta señal?",
+      respuesta: "Pare",
+      incorrectas:["Ceda el paso", "No girar a la derecha", "Límite de velocidad"],
+      informacion: "La señal de “Pare” es un símbolo universal utilizado para indicar a los conductores que deben detenerse por completo antes de proceder con seguridad."
+    },
+    {
+        imagen: "img/estacionamiento permitido.png",
+        pregunta: "¿Qué significa esta señal?",
+        respuesta: "Estacionamiento permitido",
+        incorrectas:[ "Prohibido estacionar", "No girar en U", "Zona escolar" ],
+        informacion: "La señal “Estacionamiento permitido” indica la zona habilitada para el estacionamiento o aparcamiento de vehículos."
+      },
+      {
+        imagen: "img/limite de velocidad.png",
+        pregunta: "¿Qué significa esta señal?",
+        respuesta: "Límite de velocidad",
+        incorrectas: ["Carretera resbaladiza", "Curva peligrosa", "Ceda el paso"],
+        informacion: "La señal “Límite de velocidad” establece la velocidad máxima de operación en kilómetros por hora (km/h) a la que puede circular un vehículo en determinado carril, tramo o sector de una vía."
+      },
+      {
+        imagen: "img/cruce de peatones.png",
+        pregunta: "¿Qué significa esta señal?",
+        respuesta: "Cruce de peatones",
+        incorrectas: ["Zona de obras", "Paso de animales", "No girar a la izquierda"],
+        informacion: "La señal “Cruce de peatones” habilita a que crucen los peatones y los conductores de vehículos deben detenerse para ceder el paso."
+      },
+      {
+        imagen: "img/ceda el paso.png",
+        pregunta: "¿Qué significa esta señal?",
+        respuesta: "Ceda el paso",
+        incorrectas: ["Prohibido peatones", "Zona escolar", "Cruce de peatones"],
+        informacion: "La señal “Ceda el paso” indica al conductor la obligación que tiene de ceder el paso en la próxima intersección a los vehículos que circulen por la vía a la que se aproxima o al carril al que pretende incorporarse."
+      },
+      {
+        imagen: "img/no girar en u.png",
+        pregunta: "¿Qué significa esta señal?",
+        respuesta: "No girar en U",
+        incorrectas: ["No girar a la izquierda", "No girar a la derecha", "Límite de velocidad"],
+        informacion: "La señal “No girar en U” se utiliza para indicar al conductor que no puede girar en 180 grados."
+      },
+      {
+        imagen: "img/zona escolar.png",
+        pregunta: "¿Qué significa esta señal?",
+        respuesta: "Zona escolar",
+        incorrectas: ["Paso de animales", "Cruce ferroviario", "Carretera resbaladiza"],
+        informacion: "La señal “Zona escolar” denota que el conductor deberá tomar todas las precauciones, ya que se encuentra un área muy concurrida de estudiantes."
+      }
+    
+]
