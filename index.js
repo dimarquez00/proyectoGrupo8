1a76b8533a31762fe9382a606791c41e6443713b
Diego Marquez <dimarquez@uade.edu.ar>
2024-06-14 10:53:15 -0300

    Refactor file paths for header and footer includes

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 $(function(){
   $("#idheader").load("./header.html"); 
-  $("#idheader1").load("./diegoMarquez/header1.html"); 
-  $("#idheader2").load("./camilaNani/header2.html"); 
-  $("#idheader3").load("./ianRebuffo/header3.html"); 
+  $("#idheader1").load("./header1.html"); 
+  $("#idheader2").load("./header2.html"); 
+  $("#idheader3").load("./header3.html"); 
   $("#idfooter").load("./footer.html"); 
-  $("#idfooter1").load("./diegoMarquez/footer1.html"); 
-  $("#idfooter2").load("./camilaNani/footer2.html"); 
-  $("#idfooter3").load("./ianRebuffo/footer3.html"); 
+  $("#idfooter1").load("./footer1.html"); 
+  $("#idfooter2").load("./footer2.html"); 
+  $("#idfooter3").load("./footer3.html"); 
 });
